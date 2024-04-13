a82401fb0198853e26f1a4db46fadc3bf54bd93e
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-04-13 05:04:57 +0100

    tweaks on spawning

--- Assets/Generator/LevelGenerator.cs
+++ Assets/Generator/LevelGenerator.cs
@@ -81,7 +81,6 @@ namespace Generator
 
             // Place survivors and zombies, and safe zone text
             PlaceActors();
-            PlaceSafeZoneText();
         }
 
         private void GenerateLeafIslands()
@@ -124,39 +123,48 @@ namespace Generator
         private void PlaceActors()
         {
             int distance = 10;
-            for (int i = 0; i < leafIslands.Count - 1; i++) {
+            int safeZoneIsland = Random.Range(5, leafIslands.Count);
+            for (int i = 0; i < leafIslands.Count; i++) {
                 var island = leafIslands[i];
                 var center = island.getCenter();
-                var worldCoord = GirdCoordToWorld(center.x, center.y);
-                Debug.DrawLine(new Vector3(0, 20, 0), new Vector3(worldCoord.x, 0, worldCoord.z), Color.green, 5);
-                int actorCount = Random.Range(5, 10);
-                for (int j = 0; j < actorCount; j++) {
-                    var direction = Random.insideUnitCircle;
-                    NavMeshHit hit;
-                    NavMesh.SamplePosition(new Vector3(worldCoord.x + distance * direction.x, 0, worldCoord.z + distance * direction.y), out hit, distance, NavMesh.AllAreas);
-                    if (i == 0) {
-                        var survivor = Instantiate(m_survivorPrefab, hit.position, Quaternion.identity, m_survivorsGameObject.transform);
-                        survivor.transform.position = hit.position;
-                        survivor.GetComponent<IndividualAgent>().m_gameManager = gameManager;
-                        gameManager.m_survivors.Add(survivor);
-                        gameManager.m_survivorCount++;
-                        gameManager.UpdateSurvivorCountText();
-                        break;
-                    } else {
-                        var zombie = Instantiate(m_zombiePrefab, hit.position, Quaternion.identity, m_zombiesGameObject.transform);
-                        zombie.transform.position = hit.position;
-                        zombie.GetComponent<IndividualAgent>().m_gameManager = gameManager;
-                        gameManager.m_zombies.Add(zombie);
-                        gameManager.m_zombieCount++;
-                        gameManager.UpdateZombieCountText();
+                var centerWorldCoord = GirdCoordToWorld(center.x, center.y);
+                Debug.DrawLine(new Vector3(0, 20, 0), new Vector3(centerWorldCoord.x, 0, centerWorldCoord.z), Color.green, 5);
+                if (i == 0) {
+                    PlaceActor(m_survivorPrefab, m_survivorsGameObject, centerWorldCoord, distance);
+                } else if (i != safeZoneIsland) {
+                    int zombieCount = Random.Range(5, 10);
+                    for (int j = 0; j < zombieCount; j++) {
+                        PlaceActor(m_zombiePrefab, m_zombiesGameObject, centerWorldCoord, distance);
                     }
+                } else {
+                    PlaceSafeZone(i);
                 }
             }
         }
 
-        private void PlaceSafeZoneText()
+        private void PlaceActor(GameObject prefab, GameObject collectionGameObject, Vector3 worldCoord, int distance)
         {
-            var center = leafIslands.Last().getCenter();
+            var direction = Random.insideUnitCircle;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(new Vector3(worldCoord.x + distance * direction.x, 0, worldCoord.z + distance * direction.y), out hit, distance, NavMesh.AllAreas);
+            var agent = Instantiate(prefab, hit.position, Quaternion.identity, collectionGameObject.transform);
+            agent.transform.position = hit.position;
+            agent.GetComponent<IndividualAgent>().m_gameManager = gameManager;
+            if (prefab == m_survivorPrefab) {
+                agent.transform.Rotate(0, 90, 0);
+                gameManager.m_survivors.Add(agent);
+                gameManager.m_survivorCount++;
+                gameManager.UpdateSurvivorCountText();
+            } else {
+                gameManager.m_zombies.Add(agent);
+                gameManager.m_zombieCount++;
+                gameManager.UpdateZombieCountText();
+            }
+        }
+
+        private void PlaceSafeZone(int i)
+        {
+            var center = leafIslands[i].getCenter();
             var safeZone = Instantiate(m_safeZoneTextPrefab, GirdCoordToWorld(center.x, center.y), Quaternion.identity);
             foreach (var survivor in gameManager.m_survivors) {
                 survivor.GetComponent<SurvivorAI>().m_safeZoneTransform = safeZone.transform;
